430cf2955a3d1469237f5855a01490b71a8e9c16
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-06 01:49:45 -0400

    modifying props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Chicken from './Chicken'
 import Greeter from './Greeter'
+import Die from './Die'
+
 
 function App() {
   return (
     <div>
-      <Greeter person="Abdullah" />
+      <Greeter person="Abdullah" from="Egypt" />
+      <Greeter person="Ahmed" from="Qatar" />
+      <Greeter person="Shakila" from="Sri Lanka" />
+      <Die numberSides={20} />
+      <Die numberSides={5} />
+      <Die numberSides={10} />
     </div>
   )
 }
--- src/Greeter.jsx
+++ src/Greeter.jsx
@@ -1,6 +1,6 @@
-export default function Greeter(props) {
-    console.log(props.person);
+export default function Greeter({ person, from }) {
+    console.log(person);
     return (
-        <h1>Hi There {props.person}</h1>
+        <h1>Hi There {person} --- from {from}</h1>
     )
 }
