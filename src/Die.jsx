8f5805b264edce8b773c0854972fa80c0a3d5afb
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-09 00:43:52 -0400

    Update Die.jsx

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,4 +1,4 @@
-export default function Die({ numberSides }) {
+export default function Die({ numberSides })  {
     const roll = Math.floor(Math.random() * numberSides) + 1;
     return (
         <p>Sides : {numberSides} -- Die Roll : {roll}</p>
