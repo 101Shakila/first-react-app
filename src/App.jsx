ad192f03e296780d807aeeee80cfcd1c6d2da4c0
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-09 01:52:39 -0400

    dynamic color change

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   return (
     <div>
       <Game values={[1, 2, 3, 4, 5]} />
-      {/* <ListPicker values={[1, 2, 3]} />
+      <ListPicker values={[1, 2, 3]} />
       <Greeter person="Abdullah" from="Egypt" />
       <Greeter person="Ahmed" from="Qatar" />
       <Greeter person="Shakila" from="Sri Lanka" />
       <Die numberSides={20} />
       <Die numberSides={5} />
-      <Die numberSides={10} /> */}
+      <Die numberSides={10} />
     </div>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -2,10 +2,11 @@ export default function Game({ values }) {
     const num1 = Math.floor(Math.random() * 5);
     const num2 = Math.floor(Math.random() * 5);
     let results = num1 === num2 ? "you win" : "you lose!";
+    const styles = { color: num1 === num2 ? "green" : "red" };
     if (results) {
         return (
 
-            <div>
+            <div style={styles}>
                 <p>{results}</p>
 
                 <p>Number1 : {num1}</p>
