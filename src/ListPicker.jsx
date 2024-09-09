2a5fedca4845522fd826d0b4844b8329e5c10aa3
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-09 00:59:46 -0400

    random index generator!

--- src/ListPicker.jsx
+++ src/ListPicker.jsx
@@ -1,8 +1,14 @@
 export default function ListPicker({ values }) {
+
+    const ranIndx = Math.floor(Math.random() * values.length);
+    const ranvalues = values[ranIndx];
     return (
 
-        <div>
 
+        <div>
+            <p>Array list : {values}</p>
+            <p>Random Index: {ranIndx}</p>
+            <p>Random Index: {ranvalues}</p>
         </div>
     );
 }
