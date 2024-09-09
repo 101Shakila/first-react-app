d015f8f3a24571400d409e96d7799ee6387c53d4
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-09 01:22:12 -0400

    basic condition for winner/loser

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,12 +1,23 @@
 export default function Game({ values }) {
     const num1 = Math.floor(Math.random() * 5);
     const num2 = Math.floor(Math.random() * 5);
+    if (num1 == num2) {
+        return (
 
-    return (
+            <div>
+                <p>YOU WIN!!</p>
 
-        <div>
-            <p>Number1 : {num1}</p>
-            <p>Number1 : {num2}</p>
-        </div>
-    );
+                <p>Number1 : {num1}</p>
+                <p>Number1 : {num2}</p>
+            </div>
+
+        );
+    }
+    else {
+        return (
+            <div>
+                <p>YOU LOSE!!</p>
+            </div>
+        )
+    }
 }
