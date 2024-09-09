9425d51da7a53758100bc5856972e4dc8c8b51c1
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-09 01:19:58 -0400

    update on game logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Chicken from './Chicken'
 import Greeter from './Greeter'
 import Die from './Die'
 import ListPicker from './ListPicker'
+import Game from './Game'
 
 
 function App() {
   return (
     <div>
-      <ListPicker values={[1, 2, 3]} />
+      <Game values={[1, 2, 3, 4, 5]} />
+      {/* <ListPicker values={[1, 2, 3]} />
       <Greeter person="Abdullah" from="Egypt" />
       <Greeter person="Ahmed" from="Qatar" />
       <Greeter person="Shakila" from="Sri Lanka" />
       <Die numberSides={20} />
       <Die numberSides={5} />
-      <Die numberSides={10} />
+      <Die numberSides={10} /> */}
     </div>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,12 @@
 export default function Game({ values }) {
+    const num1 = Math.floor(Math.random() * 5);
+    const num2 = Math.floor(Math.random() * 5);
+
     return (
 
         <div>
-
-
+            <p>Number1 : {num1}</p>
+            <p>Number1 : {num2}</p>
         </div>
     );
 }
