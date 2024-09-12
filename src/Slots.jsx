c32f0ba36709e5fca596cb2d0ae19f2af6d1ae55
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-12 01:06:46 -0400

    Update Slots.jsx

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,10 +1,13 @@
 export default function Slots({ val1, val2, val3 }) {
     const test = (val1 === val2 && val2 === val3);
+    const color = { color: test ? 'green' : 'red' }
+    const hiddentest = test ? false : true
     return (
         <div>
-            <h2>-- VARIABLES --</h2>
+            <h2>.MATCHING GAME.</h2>
             <p>{val1}{val2}{val3}</p>
-            <p style={{ color: test ? 'green' : 'red' }}>{test ? 'ITS A MATCH!!' : 'AWW IT FAILED!'}</p>
+            <p style={color}>{test ? 'ITS A MATCH!!' : 'AWW IT FAILED!'}</p>
+            <h3 hidden={hiddentest}>CONGRATS YOU WON!</h3>
         </div >
     )
 }
