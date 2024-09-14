f2aab6fef7a41e8849de171e9eac71ed8bbc3006
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-13 23:44:13 -0400

    cleaning up

--- src/RentalList.jsx
+++ src/RentalList.jsx
@@ -5,14 +5,14 @@ export default function RentalList({ properties }) {
         <div>
             <h1>Listing</h1>
             {
-                properties.map(p => {
+                properties.map(p => (
                     <RentalProperty
                         key={p.id}
                         name={p.name}
                         rating={p.rating}
                         price={p.price}
                     />
-                })}
+                ))}
         </div>
     )
 
@@ -20,13 +20,3 @@ export default function RentalList({ properties }) {
 }
 
 
-// const properties = [
-
-//     { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-//     { id: 129032, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-//     { id: 129033, name: "Cactus Retreat", rating: 4.75, price: 300 },
-//     { id: 129034, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-//     { id: 129035, name: "Oceanview Condo", rating: 4.7, price: 140 },
-//     { id: 129036, name: "Gold Miner campground", rating: 4.69, price: 96 }
-
-//   ];
