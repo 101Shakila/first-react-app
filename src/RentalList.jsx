7858f007e32fd201daa91cd8099b59ad7051519c
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-13 23:12:28 -0400

    rentalproperty modified

--- src/RentalList.jsx
+++ src/RentalList.jsx
@@ -1,9 +1,23 @@
 import RentalProperty from "./RentalProperty"
 
-export default function RentalList() {
+export default function RentalList({ properties }) {
     return (
-        <div></div>
+        <div>
+            <RentalProperty />
+        </div>
     )
 
 
 }
+
+
+// const properties = [
+
+//     { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//     { id: 129032, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//     { id: 129033, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//     { id: 129034, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//     { id: 129035, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//     { id: 129036, name: "Gold Miner campground", rating: 4.69, price: 96 }
+
+//   ];
--- src/RentalProperty.jsx
+++ src/RentalProperty.jsx
@@ -1,7 +1,24 @@
-export default function RentalProperty() {
+export default function RentalProperty({ name, rating, price }) {
     return (
-        <div></div>
+        <div>
+            <h1>{name}</h1>
+            <h2>{rating}</h2>
+            <h3>{price}STAR</h3>
+        </div>
     )
 
 
 }
+
+
+
+// const properties = [
+
+//     { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//     { id: 129032, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//     { id: 129033, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//     { id: 129034, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//     { id: 129035, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//     { id: 129036, name: "Gold Miner campground", rating: 4.69, price: 96 }
+
+//   ];
