a3d3320e47eeede3cdc8158eb2f4fb884c32c91d
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-14 01:02:53 -0400

    Update Clicker.jsx

--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -8,6 +8,8 @@ export default function Clicker() {
         <div>
             <h1>Click the Button!</h1>
             <button onClick={buttonClicker}>Button</button>
+            <h1>Hover Over me</h1>
+            <button onMouseOver={buttonClicker}>Button</button>
         </div>
     )
 
