04cc8112fd390e15622d75fab00ba84e61dd03fe
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-14 01:09:54 -0400

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,10 @@
+function handleForm() {
+    console.log("HANLDING FORMMMSS!");
+}
+
+
 export default function Form() {
+
     return (
         <form onSubmit={handleForm}>
             <button>Submit</button>
