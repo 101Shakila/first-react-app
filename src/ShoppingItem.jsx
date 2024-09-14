a3e277b71ff8714e2b8745aa01da8f73707a3cb0
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-13 22:38:32 -0400

    Creating more resusable components

--- src/ShoppingItem.jsx
+++ src/ShoppingItem.jsx
@@ -1,15 +1,27 @@
-export default function ShoppingItem({ list }) {
+export default function ShoppingItem({ key, item, quantity, completed }) {
+    let styles = {
+        color: completed ? 'red' : 'green',
+        textDecoration: completed ? 'line-through' : 'none'
+    };
     return (
+
+
         <div>
             <ul>
                 {
-                    list.map(i => <li key={i.id} style={{
-                        color: i.completed ? 'red' : 'green',
-                        textDecoration: i.completed ? 'line-through' : 'none'
-                    }}>
-                        {i.item} -: {i.quantity}</li>)
+                    <li key={key} style={{ styles }}> {item} -: {quantity}</li>
                 }
             </ul>
         </div >
     )
 }
+
+
+
+// {
+//     list.map(i => <li key={i.id} style={{
+//         color: i.completed ? 'red' : 'green',
+//         textDecoration: i.completed ? 'line-through' : 'none'
+//     }}>
+//         {i.item} -: {i.quantity}</li>)
+// }
--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,13 +1,18 @@
+import ShoppingItem from "./ShoppingItem"
+
 export default function ShoppingList({ list }) {
     return (
         <div>
             <ul>
                 {
-                    list.map(i => <li key={i.id} style={{
-                        color: i.completed ? 'red' : 'green',
-                        textDecoration: i.completed ? 'line-through' : 'none'
-                    }}>
-                        {i.item} -: {i.quantity}</li>)
+                    list.map(i => (
+                        <ShoppingItem
+                            key={i.id}
+                            item={i.item}
+                            quantity={i.quantity}
+                            completed={i.completed}
+                        />
+                    ))
                 }
             </ul>
         </div >
