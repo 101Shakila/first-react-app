796b42a56bec72574ab208f5b82f3cb911a39434
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-13 23:31:52 -0400

    Update RentalList.jsx

--- src/RentalList.jsx
+++ src/RentalList.jsx
@@ -3,7 +3,16 @@ import RentalProperty from "./RentalProperty"
 export default function RentalList({ properties }) {
     return (
         <div>
-            <RentalProperty />
+            <h1>Listing</h1>
+            {
+                properties.map(p => {
+                    <RentalProperty
+                        key={p.id}
+                        name={p.name}
+                        rating={p.rating}
+                        price={p.price}
+                    />
+                })}
         </div>
     )
 
