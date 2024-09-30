cc98cba393e392d3543566cacfd752494811127a
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-09-30 01:12:11 -0400

    Update ScoreKeeper.jsx

--- src/ScoreKeeper.jsx
+++ src/ScoreKeeper.jsx
@@ -12,9 +12,12 @@ function ScoreKeeper() {
     }
 
 
+    //So here we are increasing player 2 score by returning oldScore which copies the object data from earlier and returns a copy.
+    //we have to use this because the data shown score.p1Score is based on location and when click its not rerednering the entire function.
     function p2Increase() {
         console.log("you increasing p2");
         setScore((oldScore) => {
+            //Use spread operator to duplicate the values - commonly used in REACT.
             return { ...oldScore, p2Score: oldScore.p2Score + 1 }
         })
     }
