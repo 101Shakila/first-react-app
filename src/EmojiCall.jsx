c25abbb315ee7b2d35bff56183831b4e912f9927
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-10-02 10:09:46 -0400

    Update EmojiCall.jsx

--- src/EmojiCall.jsx
+++ src/EmojiCall.jsx
@@ -2,12 +2,19 @@ import { useState } from "react"
 
 
 export default function EmojiCall() {
-    const [emojis, Setemojis] = useState(["🥲"]);
+    const [emojis, SetEmojis] = useState(["🥲"]);
+
+
+    const addEmoji = () => {
+        Setemojis((oldEmoji) => [...oldEmoji, ["😌"]]);
+    }
 
     return (
         <div>
+            <h1>Emoji List</h1>
 
-
+            {emojis.map((e) => (<span >{e}</span>))}
+            <button onClick={addEmoji}>Add Emoji</button>
         </div>
     )
 }
