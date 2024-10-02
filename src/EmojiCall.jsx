d00339980a4092116aef607e5331a6657d32e2cf
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-10-02 12:18:07 -0400

    Update EmojiCall.jsx

--- src/EmojiCall.jsx
+++ src/EmojiCall.jsx
@@ -9,11 +9,17 @@ export default function EmojiCall() {
         Setemojis((oldEmoji) => [...oldEmoji, { id: uuid(), emoji: "🥹" }]);
     }
 
+    const deleteEmoji = (id) => {
+        Setemojis((prevEmoji) => {
+            return prevEmoji.filter((emojis) => emojis.id !== id);
+        })
+    }
+
     return (
         <div>
             <h1>Emoji List</h1>
 
-            {emojis.map((e) => (<span key={e.id}>{e.emoji}</span>))}
+            {emojis.map((e) => (<span onClick={() => deleteEmoji(e.id)} key={e.id}>{e.emoji}</span>))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
     )
