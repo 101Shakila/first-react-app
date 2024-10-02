880bbb88a00955af745fe2492d09c8cc837c8c4a
Shakila101 <167652608+101Shakila@users.noreply.github.com>
2024-10-02 10:20:55 -0400

    ID implementation

--- src/EmojiCall.jsx
+++ src/EmojiCall.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react"
-
+import { v4 as uuid } from "uuid"
 
 export default function EmojiCall() {
-    const [emojis, Setemojis] = useState(["🥲"]);
+    const [emojis, Setemojis] = useState([{ id: uuid(), emoji: "😍" }]);
 
 
     const addEmoji = () => {
-        Setemojis((oldEmoji) => [...oldEmoji, ["😌"]]);
+        Setemojis((oldEmoji) => [...oldEmoji, { id: uuid(), emoji: "🥹" }]);
     }
 
     return (
         <div>
             <h1>Emoji List</h1>
 
-            {emojis.map((e) => (<span >{e}</span>))}
+            {emojis.map((e) => (<span key={e.id}>{e.emoji}</span>))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
     )
